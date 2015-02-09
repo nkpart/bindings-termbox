0d427d67e5554fa808ea8df883e00e6d38807929
Nick Partridge <nkpart@gmail.com>
2015-02-10 09:26:56 +1000

    An example for the readme

--- Example.hs
+++ Example.hs
@@ -4,6 +4,8 @@ import Foreign (alloca, peek, (.|.))
 import Foreign.C.Types (CUInt)
 import Prelude hiding (length, init)
 import Data.Maybe (fromMaybe)
+import Control.Concurrent (threadDelay)
+import Data.Char (ord)
 import Bindings.Termbox
 import Data.Sequence (empty, length, Seq)
 import Control.Lens
@@ -45,3 +47,24 @@ drawSomething i c =
 
 peekEvent :: IO C'tb_event
 peekEvent = alloca $ \ptr -> c'tb_poll_event ptr >> peek ptr
+
+readmeExample :: IO ()
+readmeExample =
+  do c'tb_init
+     c'tb_clear
+     c'tb_change_cell 0 0 (ord' 'H') c'TB_BLUE c'TB_YELLOW
+     c'tb_change_cell 1 0 (ord' 'e') c'TB_BLUE c'TB_RED
+     c'tb_change_cell 2 0 (ord' 'l') c'TB_BLUE c'TB_RED
+     c'tb_change_cell 3 0 (ord' 'l') c'TB_BLUE c'TB_RED
+     c'tb_change_cell 4 0 (ord' 'o') c'TB_BLUE c'TB_RED
+     c'tb_change_cell 5 2 (ord' ' ') c'TB_DEFAULT c'TB_DEFAULT
+     c'tb_change_cell 6 2 (ord' 'W') c'TB_BLUE c'TB_YELLOW
+     c'tb_change_cell 7 2 (ord' 'o') c'TB_BLUE c'TB_RED
+     c'tb_change_cell 8 2 (ord' 'r') c'TB_BLUE c'TB_RED
+     c'tb_change_cell 9 2 (ord' 'l') c'TB_BLUE c'TB_RED
+     c'tb_change_cell 10 2 (ord' 'd') c'TB_BLUE c'TB_RED
+     c'tb_change_cell 11 2 (ord' '!') ( c'TB_BLUE .|. c'TB_BOLD ) c'TB_RED
+     c'tb_present
+     threadDelay 2000000
+     c'tb_shutdown
+  where ord' = fromIntegral . ord
